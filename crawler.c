9b047cbf3ff9bbbfe26a111f62cc949853e9aba2
Eva <evalolipop25@gmail.com>
2025-03-20 06:44:32 +0000

    part of the code for reading urls from file

--- crawler.c
+++ crawler.c
@@ -1,17 +1,39 @@
 #include <string.h> // For string manipulation
 #include <stdio.h> // i/o
 #include <curl/curl.h> // libcurl for http requests
+#include <stdlib.h> // exit() and memory functions
+
+void fetch_url(const char *url){ // function to fetch urls by taking url as input and fetch using libcurl
+    CURL *curl = curl_easy_init(); // initialize curl session (curl object)
+    if (curl){
+        printf("Fetching %s\n", url); // print url being fetched
+        curl_easy_setopt(curl, CURLOPT_URL, url); // set url for the request
+        CURLcode res = curl_easy_perform(curl); //perform http request
+
+        if(res != CURLE_OK){ // check if request failed and prints error message
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        
+        curl_easy_cleanup(curl); // cleanup curl session
+        }
+    }
+}
 
 // main
 int main(void)
 {
-    CURL *curl = curl_easy_init(); // initialize curl handle
-    if(curl) { // check if initialization successful
-        CURLcode res; // variable to store result of request
-        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com"); // setup url for request
-        res = curl_easy_perform(curl); // perform http request and store the result
-        curl_easy_cleanup(curl); // cleanup and free curl handle after use
+    FILE *file = fopen("urls.txt","r"); // open urls.txt in read mode
+    if (file == NULL){ // check if fopen failed
+        fprintf(stderr, "Error: Could not open urls.txt \n"); // print error message to standard ouput
+        return 1; // exit with error status
     }
+
+    char url[1024]; // buffer to store each line
+    while (fgets(url, sizeof(url),file)){ // read each line
+        url[strcspn(url, "\n")] = 0; // remove newline character
+        fetch_url(url); // call fetch url() to get the webpage
+    }
+    fclose(file); // close file
+    return 0; // exit successfully
 
     // 1. Input Parsing: Read URLs from a file
 
